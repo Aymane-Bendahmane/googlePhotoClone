4550b59d8fafc35367da11f29adf3f0cd349f68a
Aymane <aymane.bendahmane14@gmail.com>
2025-01-15 01:40:26 +0100

    adding multithreading to thumbnail processing

--- src/main/java/com/app/demo/GooglePhotoCloneApplication.java
+++ src/main/java/com/app/demo/GooglePhotoCloneApplication.java
@@ -4,6 +4,9 @@ package com.app.demo;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
@@ -12,21 +15,30 @@ public class GooglePhotoCloneApplication {
     static String userHome = System.getProperty("user.home");
     static Path thumbnailsDir = Path.of(userHome).resolve(".generated_thumbnails");
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         Files.createDirectories(thumbnailsDir);
         //point to the current directory where the jar exists "."
-        String directory = args.length == 1 ? args[0] : ".";
+
+        String directory = "C:\\Users\\ayman\\OneDrive\\Desktop\\GooglePhotoClone\\sample-images\\100-100-color";
         Path sourceDir = Path.of(directory);
         AtomicInteger counter = new AtomicInteger();
         long start = System.currentTimeMillis();
+
+        final ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
         try (Stream<Path> files = Files.walk(sourceDir)
                 .filter(Files::isRegularFile)
                 .filter(GooglePhotoCloneApplication::isImage)) {
             files.forEach(path -> {
-                counter.incrementAndGet();
-                new ImageMagick().createThumbnail(path, thumbnailsDir.resolve(path.getFileName()));
+                executorService.submit(() -> {
+                    counter.incrementAndGet();
+                    new ImageMagick().createThumbnail(path, thumbnailsDir.resolve(path.getFileName()));
+                });
+
             });
+            executorService.shutdown();
+            executorService.awaitTermination(1, TimeUnit.HOURS);
         }
         long end = System.currentTimeMillis();
         System.out.println(" Converted " + counter + " images to thumbnails. took " + ((end - start) * 0.001) + " seconds");
@@ -40,8 +52,6 @@ public class GooglePhotoCloneApplication {
             throw new RuntimeException(e);
         }
     }
-
-
 
 
 }
